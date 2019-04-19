65394f9e5ff2a856bae7386703a10668f0dbc4af
Kevin Plattret <kevin@plattret.me>
2019-04-19 23:33:02 +0100

    Save check-ins list to file
    It makes the data more reusable to save it in a file, all formatted.
    This does that and tidies-up the code a bit.

--- swarm.rb
+++ swarm.rb
@@ -1,5 +1,3 @@
-require "pry" # TODO remove
-require "cgi"
 require "httparty"
 require "dotenv"
 
@@ -19,6 +17,7 @@ class Swarm
       progression_count = 0
       offset = 0
       checkins_abroad = []
+      formatted_checkins = []
 
       while progression_count < total_count
         checkins = api_request(offset: offset)[:checkins][:items]
@@ -28,36 +27,39 @@ class Swarm
         checkins_abroad.push(*checkins)
       end
 
-      formatted_checkins = []
-
-      checkins_abroad.each do |checkin|
-        time = Time.at(checkin[:createdAt]).utc + (checkin[:timeZoneOffset] * 60)
-        time = time.strftime("%d/%m/%Y %H:%M")
-        venue = checkin[:venue]
-        venue = [venue[:name], venue[:location][:city], venue[:location][:country]].compact.join(", ")
-        item = [time, venue].join(" – ")
+      checkins_abroad.each do |raw_checkin|
+        checkin = serialised(raw_checkin)
+        venue = [checkin[:venue], checkin[:city], checkin[:country]].compact.join(", ")
+        item = [checkin[:time].strftime("%d/%m/%Y %H:%M"), venue].join(" – ")
         formatted_checkins << item
       end
 
-      formatted_checkins
+      File.open("./checkins-abroad.md", 'w') { |f| f.write(formatted_checkins.join("\n"))  }
     end
 
     # private
+    def serialised(checkin)
+      {
+        time: Time.at(checkin[:createdAt]).utc + (checkin[:timeZoneOffset] * 60),
+        venue: checkin[:venue][:name],
+        city: checkin[:venue][:location][:city],
+        country: checkin[:venue][:location][:country]
+      }
+    end
 
     def api_request(limit: MAX_QUERY_LIMIT, offset: 0)
       base_url = "https://api.foursquare.com/v2/users/self/checkins"
       params = "?oauth_token=#{ACCESS_TOKEN}\
-      &v=#{Time.now.utc.strftime("%Y%m%d")}\
-      &sort=newestfirst\
-      &limit=#{limit}\
-      &offset=#{offset}"
+        &v=#{Time.now.utc.strftime("%Y%m%d")}\
+        &sort=newestfirst\
+        &limit=#{limit}\
+        &offset=#{offset}"
       response = HTTParty.get(base_url + params, format: :plain)
       JSON.parse(response, symbolize_names: true)[:response]
     end
 
-    private :api_request
+    private :api_request, :serialised
   end
 end
 
-# $ ruby swarm.rb
-puts Swarm.list_checkins_abroad
+Swarm.list_checkins_abroad
